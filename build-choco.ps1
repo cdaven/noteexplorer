71c6366330bc927b61858b347f0f76bb94709402
Christian Davén <christian@daven.se>
2021-01-31 19:03:12 +0100

    Tell Rust to statically link the C runtime on Windows

--- build-choco.ps1
+++ build-choco.ps1
@@ -3,10 +3,12 @@ param([String]$version="0.0.0")
 (Get-Content .\Cargo.toml) -Replace "version = ""[^""]+""", "version = ""$version""" | Set-Content .\Cargo.toml
 (Get-Content .\Chocolatey\noteexplorer.nuspec) -Replace "<version>[^<]+</version>", "<version>$version</version>" | Set-Content .\Chocolatey\noteexplorer.nuspec
 
-cargo build --release --locked
-zip --junk-path R:\noteexplorer-win-x64-${version}.zip .\target\release\noteexplorer.exe
+$target="x86_64-pc-windows-msvc"
 
-Copy-Item .\target\release\noteexplorer.exe .\Chocolatey\tools\
+cargo build --release --offline --target=$target
+zip --junk-path R:\noteexplorer-win-x64-${version}.zip .\target\$target\release\noteexplorer.exe
+
+Copy-Item .\target\$target\release\noteexplorer.exe .\Chocolatey\tools\
 Set-Location .\Chocolatey
 choco pack
 Set-Location ..
